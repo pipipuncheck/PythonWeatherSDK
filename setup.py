ab940a007c2ff4a1afa733b953cc70c2c74eaaec
pipipuncheck <aleks.muntianu@yandex.ru>
2025-02-21 23:11:32 +0300

    test task

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='PythonWeatherSDK',
     version='0.1',
-    packages=['PythonWeatherSDK']
+    packages=['PythonWeatherSDK'],
+    install_requires=['pydantic', 'requests']
 )
